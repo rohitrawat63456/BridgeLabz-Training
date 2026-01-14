fccb24c4952f25a9c709798407a7d3769fc23bd2
rohitrawat63456 <122621142+rohitrawat63456@users.noreply.github.com>
2026-01-14 22:06:37 +0530

    [Rohit Rawat] add.added Stack and queue programms

---
+++ dsa-practice/gcr-codebase/Stack-queue-hashMap/QueueUsingStacks.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class QueueUsingStacks {
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+
+    public void enqueue(int x) {
+        s1.push(x);
+    }
+
+    public int dequeue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+        }
+
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+
+        return s2.pop();
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+              System.out.println("Queue is empty");
+        }
+
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+
+        return s2.peek();
+    }
+
+    public boolean isEmpty() {
+        return s1.isEmpty() && s2.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        QueueUsingStacks q = new QueueUsingStacks();
+
+        q.enqueue(10);
+        q.enqueue(20);
+        q.enqueue(30);
+        System.out.println("Dequeued: " + q.dequeue()); 
+        System.out.println("Front: " + q.peek());       
+        q.enqueue(40);
+        System.out.println("Dequeued: " + q.dequeue()); 
+    }
+}
